a519178b0a0ed745675324e7660b9b4d92d68435
ConstantineF <warsawxx@gmail.com>
2023-05-28 03:50:06 +0400

    basic input styling

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,6 +1,8 @@
+import "./Input.css";
+
 const Input = () => {
   return (
-    <form>
+    <form className="input-form">
       <label htmlFor="todo-input">Add your todo</label>
       <input type="text" placeholder="Buy groceries" id="todo-input" />
     </form>
--- src/components/InputWithButton/InputWithButton.tsx
+++ src/components/InputWithButton/InputWithButton.tsx
@@ -1,12 +1,14 @@
 import Input from "../Input/Input";
 import Add from "../Button/Add/Add";
 
+import "./InputWithButton.css";
+
 const InputWithButton = () => {
   return (
-    <>
+    <div className="input-wrapper">
       <Input />
       <Add />
-    </>
+    </div>
   );
 };
 
