cb2516d109ea3442baa300fbb7f61f881589f3d8
ConstantineF <warsawxx@gmail.com>
2023-06-02 02:43:32 +0400

    write a few basic tests for AddTodoForm

--- src/components/AddTodoForm.tsx/__test__/AddTodoForm.test.tsx
+++ src/components/AddTodoForm.tsx/__test__/AddTodoForm.test.tsx
@@ -1,5 +1,7 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import { Provider } from "react-redux";
+
 import store from "../../../store/store";
 
 import AddTodoForm from "../AddTodoForm";
@@ -15,15 +17,30 @@ const MockAddTodo = () => {
 describe("AddTodoForm", () => {
   it("renders without crashing", () => {
     render(<MockAddTodo />);
-
     const todoForm = screen.getByPlaceholderText(/add todo/i);
     expect(todoForm).toBeInTheDocument();
   });
 
   it("contains add button", () => {
     render(<MockAddTodo />);
-
     const addButton = screen.getByTestId("add-button");
     expect(addButton).toBeInTheDocument();
+  });
+
+  it("accepts focus on input bar", async () => {
+    const user = userEvent.setup();
+    render(<MockAddTodo />);
+    const todoForm = screen.getByPlaceholderText(/add todo/i);
+    await user.click(todoForm);
+    expect(todoForm).toHaveFocus();
+  });
+
+  it("accepts keyboard input", async () => {
+    const user = userEvent.setup();
+    render(<MockAddTodo />);
+    const todoForm = screen.getByPlaceholderText(/add todo/i);
+    await user.click(todoForm);
+    await user.keyboard("test");
+    expect(todoForm).toHaveValue("test");
   });
 });
