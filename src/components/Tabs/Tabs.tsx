e6d697c7c4493963e5a0c92a9ce531308d8db82a
ConstantineF <warsawxx@gmail.com>
2023-06-02 00:34:27 +0400

    add basic active tab highlight

--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -1,23 +1,41 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 import "./Tabs.css";
 
+const activeTabStyle = {
+  color: "var(--dark-blue)",
+};
+
 const Tabs = () => {
+  const location = useLocation().pathname;
+
   return (
     <nav className="tabs">
       <ul className="tabs-list">
         <li className="tabs-item">
-          <Link to="/all" className="tabs-link">
+          <Link
+            to="/all"
+            className="tabs-link"
+            style={location === "/all" ? activeTabStyle : {}}
+          >
             All
           </Link>
         </li>
         <li className="tabs-item">
-          <Link to="/active" className="tabs-link">
+          <Link
+            to="/active"
+            className="tabs-link"
+            style={location === "/active" ? activeTabStyle : {}}
+          >
             Active
           </Link>
         </li>
         <li className="tabs-item">
-          <Link to="/inactive" className="tabs-link">
+          <Link
+            to="/inactive"
+            className="tabs-link"
+            style={location === "/inactive" ? activeTabStyle : {}}
+          >
             Inactive
           </Link>
         </li>
