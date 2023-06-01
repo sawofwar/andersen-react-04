1c6465c0ddc2b0da918e83ff009171d9ac8d15f5
ConstantineF <warsawxx@gmail.com>
2023-06-01 05:12:46 +0400

    basic login functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,38 @@
+import { useState } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Active from "./pages/Active/Active";
-import Inactive from "./pages/Inactive/Inactive";
+
+import Login from "./pages/Login/Login";
 
 import Tabs from "./components/Tabs/Tabs";
 
-import "./App.css";
 import All from "./pages/All/All";
+import Active from "./pages/Active/Active";
+import Inactive from "./pages/Inactive/Inactive";
+
 import TaskCounter from "./components/TaskCounter/TaskCounter";
 
+import "./App.css";
+import { useEffect } from "react";
+
 function App() {
+  const [user, setUser] = useState("");
+
+  const loginSubmitHandler = (user: any) => {
+    setUser(user);
+  };
+
   return (
     <Router>
       <div className="wrapper">
         <h1>Todo List</h1>
+        <h2>Welcome {user}</h2>
 
         <Tabs />
         <Routes>
-          <Route path="/" element={<Active />} />
+          <Route
+            path="/"
+            element={<Login onSubmitLogin={loginSubmitHandler} />}
+          />
           <Route path="/inactive" element={<Inactive />} />
           <Route path="/active" element={<Active />} />
           <Route path="/all" element={<All />} />
---
+++ src/pages/Login/Login.tsx
@@ -0,0 +1,35 @@
+import { useRef } from "react";
+
+import "./Login.css";
+
+type SubmitEvent = React.FormEvent<HTMLFormElement>;
+
+const Login = ({ onSubmitLogin }: { onSubmitLogin: (a: string) => void }) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const formSubmitHandler = (e: SubmitEvent) => {
+    e.preventDefault();
+    const username = (inputRef.current as { value: string }).value;
+    onSubmitLogin(username);
+  };
+
+  return (
+    <div className="login">
+      <h1>Login</h1>
+      <form action="submit" className="login-form" onSubmit={formSubmitHandler}>
+        <label htmlFor="username">Username</label>
+        <input
+          type="text"
+          placeholder="Username"
+          className="login-input"
+          ref={inputRef}
+        />
+        <button type="submit" className="login-button">
+          Save
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
